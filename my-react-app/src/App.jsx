13b4b8cdfb59ca5be9014abefbd4aaedab1a4006
hamed-cell <attar30pro@gmail.com>
2023-10-09 16:45:22 +0200

    création du composant PokemonCard

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonCard from './components/PokemonCard';
+
+function App() {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+  );
+}
+
+export default App;
